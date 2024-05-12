f6f9e840d3fd98f3edae7337a690af3ebce60d41
YITING11129 <168101053+YITING11129@users.noreply.github.com>
2024-05-12 21:39:13 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,12 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://docs.google.com/forms/d/e/1FAIpQLSfjWFUH6pe5Jl1X00tmaoyXylTwWhRoV696i4uceKysxFV6DA/viewform?usp=sf_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLSeDB1Pd8G1O4PA0e3pJAZDBVS3ZtGiU6aE3VtcLqaTZ-SBo-g/viewform?usp=sf_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLSeLS-GvwtQtionuFdfQt6snPtpc2Gr8D7xML6HqyBHtEF4YKA/viewform?usp=sf_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLSeaQ6N8RA7zHDQqZT5G-Bik9vvKS_qUn4vrZwfZvGBL95mzgg/viewform?usp=sf_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLSddnsC2JqeOb2K3Ru2XMsE89VcViRT4WfJyOmryLdmX0_hpZQ/viewform?usp=sf_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLSd9NbMsrW_T1ARXN7uCseQifOiKoZqsv6ydqmpCAIsHV36J7A/viewform?usp=sf_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLSdSUHxwkJVWBCq_JyfrB04vlvlN7jF8Am6efxoDFbQV87exfw/viewform?usp=sf_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLSdvyeFYxwSibxOFVmRp8lD5kpjP1YFDEb6oWibeG0X5sc3ftA/viewform?usp=sf_link'
 ];
